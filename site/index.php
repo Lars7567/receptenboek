815f6a643dde501e351a7435745eac72ad409b69
Lars Mooij <mooij.lars2005@gmail.com>
2023-03-23 11:32:17 +0100

    commpit

--- site/index.php
+++ site/index.php
@@ -1,5 +1,24 @@
 <?php
 
-// Dit is het startpunt van je applicatie.
+require 'database.php';
+$sql = "SELECT * FROM recepten";
+$result = mysqli_query($conn,$sql);
+$all_recepten = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
-echo 'Welkom bij het receptenboek';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css?v=<?php echo time(); ?>">
+</head>
+<body>    
+    
+<?php  include('nav.php')?>
+
+</body>
+</html>
---
+++ site/nav.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<nav class="nav_background">
+        <label><img src="LogoMakr-117Nbi.png" alt="image not found" class="Logo-width"><a class="titel"></a></label>
+        <ul>
+            <li><a class="active" href="#">HOME</a></li>
+            <li><a href="#">Speciaal</a></li>
+        </ul>
+    </nav>
+</body>
+</html>
